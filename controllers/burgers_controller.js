923594c3124a4153378bd38cccbb3d15ec744af4
Jessica C <jgcreiglow@gmail.com>
2018-03-20 10:11:22 -0400

    removed typo

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,7 +5,7 @@ var router = express.Router();
 var burger = require('../models/burger.js');
 
 router.get("/", function (req, res) {
-  burger.all(function (data) {
+  burgers.all(function (data) {
     var hbsObject = {
       burgers: data
     };
